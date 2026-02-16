0dad9234b52c143980c950f8c3f6fd902bde654e
Mikhail Pankin <mishapankin@gmail.com>
2026-02-16 15:41:26 +0300

    examples: better rock example

--- examples/2_properties/scene.py
+++ examples/2_properties/scene.py
@@ -1,6 +1,9 @@
 import rv
 import random
 
+light_base = (0.93, 0.92, 0.91)
+dark_base = (0.12, 0.08, 0.07)
+
 
 class BasicScene(rv.Scene):
     def generate(self):
@@ -9,15 +12,16 @@ class BasicScene(rv.Scene):
         rock = rock_loader.create_instance()
         rock.set_property("geo_seed", random.uniform(0, 1000))
         rock.set_property(
-            "color",
+            "highlight_color",
             [
                 0.35 * random.uniform(0.9, 1.1),
                 0.25 * random.uniform(0.9, 1.1),
                 0.2 * random.uniform(0.9, 1.1),
             ],
         )
+        rock.set_property("color_base", random.choice([light_base, dark_base]))
 
         plane = self.create_plane(size=100).set_location([0, 0, 0])
-        empty = self.create_empty().set_location((0, 0, 0))
+        empty = self.create_empty().set_location((0, 0, 0.1))
 
-        cam = self.get_camera().set_location((2, 0, 0.7)).point_at(empty)
+        cam = self.get_camera().set_location((1, 0, 0.2)).point_at(empty)
