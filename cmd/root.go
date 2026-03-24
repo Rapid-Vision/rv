5ee87cf24069738b986a331426b24d5f562721d8
Mikhail Pankin <mishapankin@gmail.com>
2026-03-24 12:11:02 +0300

    version: 0.2.0

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "rv",
 	Short:   "A lightweight tool for generating labeled synthetic image datasets with just a few commands.",
-	Version: "0.1.0",
+	Version: "0.2.0",
 }
 
 func Execute() {
